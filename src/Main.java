0e0065ba54249e959080993ca8938bc4e53c85c1
Tatiana Bazhanova <bazya_t@mail.ru>
2022-09-01 17:21:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5000; // стоимость билета
+        int rouble = 20; // количество рублей для одной бонусной милли
+
+        System.out.println("Итого:");
+        System.out.println((ticket / rouble) + " бонусных миль");
+    }
+}
