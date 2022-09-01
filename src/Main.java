6022e2c188ced740fd32586b4da7aca8c7af1cab
Tatiana Bazhanova <bazya_t@mail.ru>
2022-09-01 17:52:55 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 5000; // стоимость билета
-        int rouble = 20; // количество рублей для одной бонусной милли
+        System.out.println("Бесплатные мили за перелёты");
+        System.out.println("За каждые 20 рублей, потраченные на билет, начисляется 1 миля");
 
+        int ticket = 10000; // стоимость билета
+        int rouble = 20; // количество рублей для одной бонусной милли
+        System.out.println((ticket) + " Стоимость билета");
         System.out.println("Итого:");
         System.out.println((ticket / rouble) + " бонусных миль");
     }
